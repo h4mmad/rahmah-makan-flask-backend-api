577fd94ddcd72706a91604dac374abfbb68a53cf
h4mmad <mohammedhammad07@gmail.com>
2023-01-06 13:32:49 +0300

    added usage number

--- app.py
+++ app.py
@@ -12,9 +12,23 @@ def index():
 
     objects_num, labels, img, bnd_box, cnfdce = rahmah_makan_vision.get_objects_num_with_labels()
 
+    usage = 0
+    if objects_num <= 4 and objects_num >= 0:
+        usage = 1
+    if objects_num > 4 and objects_num <= 6:
+        usage = 2
+    if objects_num > 6:
+        usage = 3
+
+    #usage 
+    #1 corresponds to not full
+    #2 corresponds to almost full
+    #3 corresponds to completely full
+
     return jsonify(
         objects_num = objects_num,
-        labels = labels
+        labels = labels,
+        usage = usage
     )
 
 
